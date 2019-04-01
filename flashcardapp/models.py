f17123001c636fcfc538e1f0f395bccccf8f6f64
aaronsellek <aaron.sellek1@gmail.com>
2019-04-01 15:18:25 -0400

    updates

--- flashcardapp/models.py
+++ flashcardapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Card(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="cards")
+    question = models.TextField(blank=False, null=False)
+    answer = models.TextField(blank=False, null=False)
+    answer_right = models.IntegerField(default="0")
+    answer_wrong = models.IntegerField(default="0")
+
+class Quiz(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="quizzes")
+    title = models.CharField(max_length=255)
+    date_created = models.DateTimeField(auto_now_add=True)
