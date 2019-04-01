5fe9c36103a40aa695d024c48bc6a65ba4afc407
aaronsellek <aaron.sellek1@gmail.com>
2019-04-01 14:48:51 -0400

    added to models and added registration redux

--- core/models.py
+++ core/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Card(models.Model):
-    front = models.CharField(max_length=255)
-    back = models.CharField(max_length=255)
-    user = models.ForeignKey(User)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="cards")
+    question = models.TextField(blank=False, null=False)
+    answer = models.TextField(blank=False, null=False)
+    answer_right = models.IntegerField(default="0")
+    answer_wrong = models.IntegerField(default="0")
+
+class Quiz(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="quizzes")
+    title = models.CharField(max_length=255)
+    date_created = models.DateTimeField(auto_now_add=True)
