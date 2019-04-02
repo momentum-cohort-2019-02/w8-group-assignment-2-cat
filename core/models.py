fb68dc90793d6aed67c564f2036eb4fc8b549331
aaronsellek <aaron.sellek1@gmail.com>
2019-04-02 13:29:30 -0400

    my computer hates me

--- core/models.py
+++ core/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Quiz(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="quizzes")
+    title = models.CharField(max_length=255)
+    date_created = models.DateTimeField(auto_now_add=True)
+
 class Card(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="cards")
     question = models.TextField(blank=False, null=False)
@@ -8,7 +13,5 @@ class Card(models.Model):
     answer_right = models.IntegerField(default="0")
     answer_wrong = models.IntegerField(default="0")
 
-class Quiz(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="quizzes")
-    title = models.CharField(max_length=255)
-    date_created = models.DateTimeField(auto_now_add=True)
+def __str__(self):
+        return self.quiz
