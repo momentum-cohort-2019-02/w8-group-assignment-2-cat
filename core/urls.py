d29c0467150f7ceef48470bfac8f0b388517ecb9
Taylor Cooke <taylorcooke@gmail.com>
2019-04-03 16:32:59 -0400

    create quiz link works, takes to page for submitting q and a

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', core_views.index, name='index'),
     path('profile/<username>', core_views.get_user_profile, name="user_profile"),
     path('quiz/<int:pk>/create_card', core_views.card_new, name="create-card"),
-    path('quiz/<int:pk>', core_views.quiz_new, name="create-quiz")
+    path('quiz/', core_views.quiz_new, name="create-quiz")
 
 ]
