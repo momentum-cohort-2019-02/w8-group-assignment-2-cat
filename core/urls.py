4b3751e4419e386d55f6de914d2034ad02e36d7d
Taylor Cooke <taylorcooke@gmail.com>
2019-04-04 12:15:44 -0400

    user page works

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from core import views as core_views
 
 urlpatterns = [
     path('', core_views.index, name='index'),
-    path('profile/<username>', core_views.get_user_profile, name="user_profile"),
+    path('profile/<username>', core_views.user_profile, name="user_profile"),
     path('quiz/<int:pk>/create_card', core_views.card_new, name="create-card"),
     path('quiz/', core_views.quiz_new, name="create-quiz")
 
