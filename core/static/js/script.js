9aa48fce9a755ad6ee66a336b3e9e8d898042d5f
Christian <ChristianLMedlin@gmail.com>
2019-04-08 21:21:44 -0400

    Frontside of card JavaScript now working

--- core/static/js/script.js
+++ core/static/js/script.js
@@ -1,47 +1,65 @@
 document.addEventListener("DOMContentLoaded", function() {
+  //Currently used until Json is properly imported.
   let dummyData = {
     "What is the capital of North Carolina?": "Raleigh",
     "Who is the lead instructor of Momentum?": "Clinton",
     "What does HTML stand for?": "Hyper-text Markup Language",
     "Who was Django named after?": "Django Reinhardt",
   };
+
   let dummyKey = 0;
   // let dummyValue = 0;
-  
-  
-  
-  let flashcard_child = `
-      <div class="flashcardChild">
-        <p>
-          ${Object.keys(dummyData)[dummyKey]}
-        </p>
-        <button class="display_answer_button">
-  
-        </button>        
-      </div>`;
 
+  var answer_button = document.querySelector(".startTheGame");
+  let flashcard_parent = document.querySelector(".flashcardParent");
 
-  let flashcard_parent = document.getElementsByClassName("flashcardParent");
-  let answer_button = document.getElementsByClassName("display_answer_button");
-  let start_game = document.querySelector(".startTheGame");
+  
+  let flashcard_child = 
+ //Creates the front of the card, this will iterate through all available questions.
 
-  function card_function(flashcard_parent, flashcard_child){
-    flashcard_parent.insertAdjacentHTML('beforeend', flashcard_child)
-    console.log('Hello')
+  `<div class="flashcardChild">
+    <p>
+      ${Object.keys(dummyData)[dummyKey]}
+    </p>
+    <button class="display_answer_button">
+      Next
+    </button>        
+  </div>`;
+
+  
+  function game_starter(){
+    //Contains the functionality for displaying the contents of the front string literal. 
+    //Calls queryAssigner to assign querySelectors to created items.
+
+    answer_button.addEventListener("click", function(){  flashcard_parent.insertAdjacentHTML('beforeend', flashcard_child);
+    console.log('Hello');
+    queryAssigner(answer_button, flashcard_parent)});
+    console.log("Is this running?");
   };
 
-console.log(start_game)
 
-start_game.addEventListener("click", function() {card_function(flashcard_parent, flashcard_child)});
+
+  // function card_function(answer_button, flashcard_parent, flashcard_child){
+  //   flashcard_parent.insertAdjacentHTML('beforeend', flashcard_child)
+  //   console.log('Hello')
+  //   queryAssigner(answer_button, flashcard_parent)
+  // };
   
-function answer_button_function(){answer_button.addEventListener("click", card_function(flashcard_parent, flashcard_child))};
-
-  
 
 
+  function queryAssigner(answer_button, flashcard_parent){
+    //Attaches proper querySelectors to objects created by the string literal.
 
+    console.log("Goodbye")
+    answer_button = document.querySelector(".display_answer_button")
+    answer_button.addEventListener("click", game_starter(flashcard_parent, flashcard_child))
+  };
+
+game_starter();
 });
   
+
+
  
 
 
